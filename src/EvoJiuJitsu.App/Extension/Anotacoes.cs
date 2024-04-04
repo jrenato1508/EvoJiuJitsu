bfd29554ffcf0ec970c43507d66e67877b35c48e
jrenato1508 <jrenato1508@gmail.com>
2024-04-04 15:35:50 -0300

    Adicionando a class Anotação

---
+++ src/EvoJiuJitsu.App/Extension/Anotacoes.cs
@@ -0,0 +1,22 @@
+﻿namespace EvoJiuJitsu.App.Extension
+{
+    public class Anotacoes
+    {
+        // Class Criada com o proposito de registrar anotações e informações sobre o desenvolvimento do software
+
+        #region DATATABLES - PAGINAÇÃO
+        /*
+         
+         dicionando o DataTables 
+         Adicionar o DataTable(paginação) Link - https://www.youtube.com/watch?v=2UXNZobzWKQ
+         Site Oficial: https://datatables.net/
+
+         ONDE USAMOS?
+         Usamos na pagina html index da View Atleta.
+
+         Configuração?
+         Na pagina _Layout e na pagina index de atleta
+         */
+        #endregion
+    }
+}
